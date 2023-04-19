fe7ca3c81efc4c2f8015cf099276b03142a67baa
Marcos <mrodriguezgarciamiguel@gmail.com>
2023-04-19 13:31:01 +0200

    guardar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, introduce tu nombre");
-var nombre = Console.ReadLine;
+var nombre = Console.ReadLine();
+
+var nombreInvertido= "";
+Console.Write("Adiós " + nombreInvertido);
+
+for (int i = (nombre.Length-1); i >= 0; i--)
+{
+    Console.Write(nombre[i]);
+}
